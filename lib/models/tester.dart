c202c3df3e97a026b24b82c2d1df4b682a7881fc
Peter Wauyo <pbwauyo@gmail.com>
2020-11-13 11:55:35 +0300

    added more to the test invitation repo

---
+++ lib/models/tester.dart
@@ -0,0 +1,26 @@
+class Tester {
+  String name;
+  String email;
+  String phone;
+  String invitationCode;
+
+  Tester({this.name, this.email, this.phone, this.invitationCode});
+
+  factory Tester.fromMap(Map<String, dynamic> map){
+    return Tester(
+      name: map["name"],
+      email: map["email"],
+      phone: map["phone"],
+      invitationCode: map["invitationCode"]
+    );
+  }
+
+  Map<String, dynamic> toMap(){
+    return {
+      "name" : name,
+      "email" : email,
+      "phone" : phone,
+      "invitationCode" : invitationCode
+    };
+  }
+}
--- lib/repos/test_invitation_repo.dart
+++ lib/repos/test_invitation_repo.dart
@@ -1,4 +1,12 @@
+import 'package:auth_app/models/tester.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
 class TestInvitationRepo {
+  final _firestore = FirebaseFirestore.instance;
+
+  CollectionReference get _testersCollectionRef => _firestore.collection("testers");
+
   bool verifyInvitationCode(String invitationCode){
     final splitCode = invitationCode.split("-");
     if(splitCode.length != 2){
@@ -9,5 +17,21 @@ class TestInvitationRepo {
       }
     }
     return true;
+  }
+
+  Future<Tester> getTester(String email) async{
+    final querySnapshot = await _testersCollectionRef.where("email", isEqualTo: email).get();
+    final doc = querySnapshot.docs.first;
+    return Tester.fromMap(doc.data());
+  }
+
+  Future<bool> testerExists({@required String email, @required String invitationCode}) async{
+    final querySnapshot = await _testersCollectionRef.where("email", isEqualTo: email)
+                                            .where("invitationCode", isEqualTo: invitationCode).get();
+    return querySnapshot.docs.first.exists;
+  }
+
+  Future<void> saveTester(Tester tester) async{
+    await _testersCollectionRef.add(tester.toMap());
   }
 }
